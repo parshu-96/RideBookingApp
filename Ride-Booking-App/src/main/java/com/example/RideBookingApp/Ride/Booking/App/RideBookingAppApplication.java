0d78457844bf1f270b7b0fc917d53d77db98b99a
parshu <parshuvmahindrakar@gmail.com>
2025-07-08 20:13:35 +0530

    Added required starters

---
+++ Ride-Booking-App/src/main/java/com/example/RideBookingApp/Ride/Booking/App/RideBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RideBookingApp.Ride.Booking.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideBookingAppApplication.class, args);
+	}
+
+}
---
+++ Ride-Booking-App/src/test/java/com/example/RideBookingApp/Ride/Booking/App/RideBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RideBookingApp.Ride.Booking.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
